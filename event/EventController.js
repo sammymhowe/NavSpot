9cf51d6181c1d340d9f58fc48f4581f463782c84
sammymhowe <sammymhowe@gmail.com>
2017-04-15 11:31:06 -0400

    Created GET method, needs to be implemented

--- event/EventController.js
+++ event/EventController.js
@@ -11,21 +11,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // TODO: Redirect to the same page with a blank form after Submit
 // Packages the data in the form and sends it as a JSON object
 app.post('/dashboard', function(req, res) {
-  res.send(req.body); // Display the JSON object form data on a new screen
-  db.collection('test1').insertOne( {
-     "eventName" : {
-       eventName : req.body.eventName
-     },
-     "eventType"  : {
-     eventType : req.body.eventType
-    },
-      "eventLocation" : {
-    eventLocation : req.body.eventLocation
-  },
-    "eventDetails" :  {
-      eventDetails : req.body.eventDetails
-    }
-   });
+  var newEvent = req.body;
+
+  // The if/else statement is commented out for now to make debugging easier
+  // TODO: Figure out whether to do validations here or somewhere else
+  // if (!(req.body.eventName)) {
+  //   error(res, "Must provide an event name.", 400);
+  // }
+  // else {
+    db.collection('events').insertOne(newEvent, function(err, doc) {
+      if (err) {
+        error(res, "Failed to create new event.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  // }
 });
+
+// GET
+app.get('/dashboard', function(req, res){
+  db.collection(events).find({}).toArray(function(err, docs) {
+    if (err) {
+      error(res, "Failed to get events.");
+    }
+    else {
+      res.status(200).jason(docs);
+    }
+  });
+});
+
+// Function for erroring
+function error(res, message, code) {
+  console.log("ERROR: " + message);
+  res.status(code || 500).json({"error": message});
+}
+
 
 module.exports = app; // Goes at the end to export the router to be used in app.js
--- server.js
+++ server.js
@@ -4,4 +4,6 @@ var app = require('./app');
 var http = require('http');						// Basic HTTP functionality
 var port = 8080;
 var server = http.Server(app);
-server.listen(port); // Start the server
+server.listen(port, function() {
+  console.log("Server now running on port " + port);
+});
